261fd8c547c69a569fd751f0971bd97693f7adb6
Domenico Martella <domenico.martella@alcacoop.it>
2013-09-29 18:16:03 +0200

    REFACTORING: Checker extends Image from Group

--- fourinaline/src/it/alcacoop/fourinaline/actors/Checker.java
+++ fourinaline/src/it/alcacoop/fourinaline/actors/Checker.java
@@ -2,75 +2,31 @@ package it.alcacoop.fourinaline.actors;
 
 import it.alcacoop.fourinaline.FourInALine;
 
-import com.badlogic.gdx.scenes.scene2d.Group;
-import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Pool.Poolable;
 
-public class Checker extends Group implements Poolable {
+public class Checker extends Image implements Poolable {
 
-  private Image i1, i2, hi;
+  TextureRegion r1, r2;
   
   public Checker() {
-    i1 = new Image(FourInALine.Instance.atlas.findRegion("CSW"));
-    i2 = new Image(FourInALine.Instance.atlas.findRegion("CSB"));
-    hi = new Image(FourInALine.Instance.atlas.findRegion("hi"));
-    addActor(i1);
-    addActor(i2);
-    addActor(hi);
-    hi.addAction(Actions.forever(Actions.sequence(
-      Actions.fadeOut(0.2f),
-      Actions.fadeIn(0.2f)
-    )));
+    super();
+    r1 = FourInALine.Instance.atlas.findRegion("CSW");
+    r2 = FourInALine.Instance.atlas.findRegion("CSB");
+    setDrawable(new TextureRegionDrawable(r1));
   }
 
   public void setColor(int color) {
-    hi.setVisible(false);
-    if (color==1) {
-      i1.setColor(1,1,1,1);
-      i2.setColor(1,1,1,0);
-    }
-    if (color==2) {
-      i1.setColor(1,1,1,0);
-      i2.setColor(1,1,1,1);
-    }
+    if (color==1)
+      setDrawable(new TextureRegionDrawable(r1));
+    if (color==2)
+      setDrawable(new TextureRegionDrawable(r2));
   }
 
-  public void highlight() {
-    hi.setVisible(true);
-  }
-  
-  
-  @Override
-  public void setWidth(float width) {
-    i1.setWidth(width);
-    i2.setWidth(width);
-    hi.setWidth(width);
-  }
-  
-  @Override
-  public void setHeight(float height) {
-    i1.setHeight(height);
-    i2.setHeight(height);
-    hi.setHeight(height);
-  }
-  
-  @Override
-  public float getWidth() {
-    return i1.getWidth();
-  }
-  
-  @Override
-  public float getHeight() {
-    return i1.getHeight();
-  }
-  
   @Override
   public void reset() {
-    i1.setColor(1,1,1,0);
-    i2.setColor(1,1,1,0);
-    hi.setVisible(false);
-    setColor(1,1,1,1);
+    setColor(1, 1, 1, 1);
   }
-
 }
