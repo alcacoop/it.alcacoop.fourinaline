0770dcbdd68976a950f048e9232294feaa588b84
Domenico Martella <domenico.martella@alcacoop.it>
2013-10-02 12:01:04 +0200

    FEATURE: main menu screen

--- fourinaline/src/it/alcacoop/fourinaline/layers/MenuScreen.java
+++ fourinaline/src/it/alcacoop/fourinaline/layers/MenuScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
@@ -19,18 +20,72 @@ public class MenuScreen extends BaseScreen {
   
   public MenuScreen() {
     table = new Table();
-    table.setWidth(stage.getWidth()*0.9f);
+    table.setWidth(stage.getWidth()*0.7f);
     table.setHeight(stage.getHeight()*0.9f);
+    
+    float height = table.getHeight()/7;
+    float width = table.getWidth()/2;
+    float pad = height/55;
+    
     table.setPosition((stage.getWidth()-table.getWidth())/2, (stage.getHeight()-table.getHeight())/2);
     
-    TextButton sp = new TextButton("Single Play", FourInALine.Instance.skin);
-    sp.addListener(new ClickListener(){
-      @Override
+    ClickListener cl = new ClickListener() {
       public void clicked(InputEvent event, float x, float y) {
-        FourInALine.Instance.fsm.processEvent(Events.BUTTON_CLICKED, null);
-      }
-    });
-    table.add(sp);
+        FourInALine.Instance.fsm.processEvent(Events.BUTTON_CLICKED,((TextButton)event.getListenerActor()).getText().toString().toUpperCase());
+      };
+    };
+    
+    
+    TextButton sp = new TextButton("Single Player", FourInALine.Instance.skin);
+    sp.addListener(cl);
+    TextButton tp = new TextButton("Two Players", FourInALine.Instance.skin);
+    tp.addListener(cl);
+    TextButton gp = new TextButton("Online Multiplayer", FourInALine.Instance.skin);
+    gp.addListener(cl);
+    
+    TextButton op = new TextButton("Options", FourInALine.Instance.skin);
+    op.addListener(cl);
+    
+    TextButton ri = new TextButton("Rate it!", FourInALine.Instance.skin);
+    ri.addListener(cl);
+    TextButton ab = new TextButton("About..", FourInALine.Instance.skin);
+    ab.addListener(cl);
+    
+    table.add(new Image(FourInALine.Instance.atlas.findRegion("logo"))).expand().colspan(2);
+    
+    table.row().pad(pad);
+    table.add().colspan(2).fill().expand();
+    
+    table.row().pad(pad);
+    table.add(sp).fill().expandX().height(height).colspan(2);
+    
+    table.row().pad(pad);
+    table.add(tp).fill().expandX().height(height).width(width);
+    table.add(gp).fill().expandX().height(height).width(width);
+    
+    table.row();
+    table.add().colspan(2).fill().expand();
+    
+    table.row().pad(pad);
+    table.add(op).fill().expandX().height(height).colspan(2);
+    
+    
+    table.row();
+    table.add().colspan(2).fill().expand();
+    
+    table.row().pad(pad);
+    table.add(ri).fill().expandX().height(height).width(width);
+    table.add(ab).fill().expandX().height(height).width(width);
+    
+    if (true) { //TODO: isPremium
+      TextButton ad = new TextButton("Remove Ads", FourInALine.Instance.skin);
+      ad.addListener(cl);
+      table.row().pad(pad);
+      table.add(ad).fill().expandX().height(height).colspan(2);
+    }
+    
+    table.row();
+    table.add().colspan(2).fill().expand();
     
     stage.addActor(table);
   }
