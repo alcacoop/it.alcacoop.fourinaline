6b4b6330008d05e93c5848305904789002a5fbe3
Domenico Martella <domenico.martella@alcacoop.it>
2013-10-02 11:58:35 +0200

    FIX: disabled GL20

--- fourinaline-desktop/src/it/alcacoop/fourinaline/FourInALineDesktop.java
+++ fourinaline-desktop/src/it/alcacoop/fourinaline/FourInALineDesktop.java
@@ -4,13 +4,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class FourInALineDesktop {
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "fourinaline";
-		cfg.useGL20 = false;
-		cfg.width = 800;
-		cfg.height = 480;
-		
-		new LwjglApplication(new FourInALine(), cfg);
-	}
+  public static void main(String[] args) {
+    LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+    cfg.title = "fourinaline";
+    cfg.useGL20 = false;
+    cfg.width = 800;
+    cfg.height = 480;
+
+    new LwjglApplication(new FourInALine(), cfg);
+  }
 }
